275105e4b8e55e0765d002f97c6122212785d7e9
dmousmane <dmousmane@gmail.com>
2016-10-12 12:01:38 +0200

    suite projet java

--- workspace/Tetris/src/MainPrincipal.java
+++ workspace/Tetris/src/MainPrincipal.java
@@ -3,82 +3,23 @@ import java.util.Scanner;
 
 public class MainPrincipal {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		int cpt = 0;
+		Scanner sc = new Scanner(System.in);
 		Fabrique f = new Fabrique();
 		Piece p = f.creerCarre();
 		Plateau plat = new Plateau();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		p = p.versLeBas();
-		plat.jouer(p);
-		//p = p.versLeBas();
-		//plat.jouer(p);
+		
+		
+		while(true){
+			while(!plat.jeuPerdu(p)){
+				System.out.println( "'''''");
 
-		if(!plat.accepter(p.versLeBas())){
-			p = f.creerCarre();
-			plat.jouer(p);
-		}
-		
-		p = p.versLeBas();
-		plat.jouer(p);
-		
-		plat.verifieLigne();
-		
-		
-		
-		
-		
-	
-		
-		
-		
-		
-
-		
-		/*while(true){
-			while(!plat.bloquer(p)){
-				cpt++;
 				int dep = sc.nextInt();
 				if(dep == 1)
 				{
 				p = p.versLeBas();
 				}
-				else if(dep == 2 && plat.bloquer(p)){
+				else if(dep == 2){
 					System.out.println("####");
 					p = p.getAncienne().getAncienne();
 				}
@@ -92,13 +33,42 @@ public class MainPrincipal {
 				}
 				plat.jouer(p);
 			}
-			if(plat.bloquer(p)){
+			if(plat.jeuPerdu(p)){
 				Fabrique ff = new Fabrique(); 
 				p = ff.creerCarre();
 			}	
-		}*/
+		}
 		
-	
+		/*boolean jeu = false;
+		while(jeu == false){
+			plat.jouer(p);
+			plat.retirer(p);
+			p = p.versLeBas();
+			if(plat.accepter(p)){
+				int dep = sc.nextInt();
+				if(dep == 1){
+					p = p.versLaGauche();
+				}
+				else if(dep == 0){
+					p = p.versLeBas();
+				}
+				else if(dep == 3){
+					p = p.versLaDroite();
+				}
+				else if(dep==3)
+				{
+					p = p.versLaGauche();
+				}
+			}
+			else{
+				Fabrique ff = new Fabrique();
+				System.out.println("--"+p.getAncienne().getCelluleUn().getY());
+				p = ff.creerCarre();
+				System.out.println("----"+p.getCelluleUn().getY());
+				jeu = plat.jeuPerdu(p);
+			}
+			System.out.println(jeu);
+		}
+		//sc.close();*/
 	}
-
 }
