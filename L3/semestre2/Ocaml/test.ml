1df19c460004b6825f79ba74eca7444ed8be7141
Dmousmane_fac <dmousmane@gmail.com>
2018-01-17 15:15:26 +0100

    suite caml

--- L3/semestre2/Ocaml/test.ml
+++ L3/semestre2/Ocaml/test.ml
@@ -1,4 +1,4 @@
-(*let rec puissance x =
+let rec puissance x =
   if x>10 then ()
   else
   begin
@@ -12,15 +12,21 @@ print_string("entrez un entier : ");;
 let x = read_int();;
 print_string("resutat");;
 print_newline();;
-puissance x;;*)
+puissance x;;
 
-(*--------------------------------------------------------------*)
+(*--------------------------------------------------------------
 
 let rec ackermann m n =
   if m = 0 then n+1
-  else if (n=0 && m>0) then ackermann m-1 1
-  else ackermann m-1 ackermann m n-1 
+  else if (n=0 && m>0) then ackermann (m-1) 1
+  else ackermann (m-1) (ackermann m (n-1))
 ;;
 
-let x = Ackermann(2,1);;
+print_string("entrez un premier entier svp :");;
+let x = read_int();;
+print_string("entrez un second entier svp :");;
+let y = read_int();;
+let x = ackermann x y;;
+print_string("resultat : ");;
 print_int(x);;
+print_newline();;*)
